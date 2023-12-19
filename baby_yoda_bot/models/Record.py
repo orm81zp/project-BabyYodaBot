ceb9b868b0086e7f837fc74617c9cc22b779b3b0
antonina <atoninasych@gmail.com>
2023-12-19 14:47:36 +0000

    updated Record.py

--- baby_yoda_bot/models/Record.py
+++ baby_yoda_bot/models/Record.py
@@ -1,14 +1,6 @@
 from .Phone import Phone
 from .Name import Name
 from .Email import Email
-from .Birthday import Birthday
-
-# todo
-
-# add_phone, show_phone, change_phone, remove_phone
-# add_email, show_email, remove_email
-# add_birthday, show_birthday, remove_birthday
-# add_address, show_address, remove_address
 
 
 class Record:
@@ -20,25 +12,26 @@ class Record:
         self.address = None
 
     # ----------- EMAIL------------------------------------------------
-    def add_email(self, email):
+    def add_address(self, email):
         self.email = Email(email)
 
-    def show_email(self):
+    def show_address(self):
         return self.email
 
-    def remove_email(self):
+    def remove_address(self):
         self.email = None
 
     # ----------- PHONE------------------------------------------------
     def add_phone(self, number):
         self.phones.append(Phone(number))
 
-    def show_phone(self):
-        return self.phones
+    def show_phones(self):
+        phone_numbers = ", ".join(str(phone) for phone in self.phones)
+        return phone_numbers
 
-    def edit_phone(self, old_phone, new_phone):
+    def change_phone(self, old_phone, new_phone):
         for i in range(len(self.phones)):
-            if self.phones[i] == old_phone:
+            if str(self.phones[i]) == str(old_phone):
                 self.phones[i] = Phone(new_phone)
 
     def find_phone(self, search_phone):
@@ -47,10 +40,23 @@ class Record:
                 return phone
         raise ValueError("Phone not found")
 
+    def remove_phone(self, search_phone):
+        try:
+            phone = self.find_phone(search_phone)
+            self.phones.remove(phone)
+        except Exception:
+            raise KeyError("Phone not found")
+
     # ----------- BIRTHDAY----------------------------------------------
 
-    def add_birthday(self, birthday: Birthday):
-        self.birthday = Birthday(birthday)
+    def add_birthday(self, birthday):
+        self.birthday = birthday
+
+    def show_birthday(self):
+        return self.birthday
+
+    def remove_birthday(self):
+        self.birthday = None
 
     def __str__(self):
         phone_numbers = ", ".join(str(phone) for phone in self.phones)
--- test_baby_yoda.py
+++ test_baby_yoda.py
@@ -1,26 +1,9 @@
 from baby_yoda_bot.main import yoda_say
-from baby_yoda_bot.models import Name
-from baby_yoda_bot.models import Phone
-from baby_yoda_bot.models import Email
-from baby_yoda_bot.models import Birthday
-from baby_yoda_bot.models import Record
-
-#
-# def main():
-#     yoda_say()
-#
-#
-# if __name__ == "__main__":
-#     main()
 
 
-name = Name("Antonina")
-print(name)
-phone = Phone("+12123456789098")
-print(phone)
-email = Email("antonina@gmail.com")
-print(email)
-birthday = Birthday("23.03.2021")
-print(birthday)
-# record = Record("Alex")
-# record.add_phone('+1234567890981')
+def main():
+    yoda_say()
+
+
+if __name__ == "__main__":
+    main()
