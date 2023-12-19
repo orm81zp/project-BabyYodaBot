13e79a382b784380c23bab0f5dfe12584bd99bb3
Roman Onishchenko <orm81zp@gmail.com>
2023-12-19 14:21:24 +0200

    update message

--- baby_yoda_bot/models/Birthday.py
+++ baby_yoda_bot/models/Birthday.py
@@ -13,7 +13,7 @@ class Birthday(Field):
     def value(self, new_value):
         # check format DD.MM.YYYY
         if not new_value or not re.search(r"^\d{2}\.\d{2}\.\d{4}$", new_value):
-            raise ValidationValueException("Birthday failed validation")
+            raise ValidationValueException("Birthday failed validation.")
 
         # check validation on correct day, month, year arguments
         try:
@@ -22,11 +22,13 @@ class Birthday(Field):
                 year=int(year), month=int(month), day=int(day)
             )
         except Exception:
-            raise ValidationValueException("Birthday failed validation")
+            raise ValidationValueException("Birthday failed validation.")
 
         # check that the birthday is not in the future
         if birthday_date > datetime.date.today():
-            raise ValidationValueException("Birthday can be in the future")
+            raise ValidationValueException(
+                "Birthday failed validation. The future's date of birth is not accepted"
+            )
 
         self._value = new_value
 
