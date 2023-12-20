2e00a501ef7897c49a0b3e898e39fd7d3423f6c6
VPavelko <shadows0breath@gmail.com>
2023-12-20 21:38:29 +0200

    add bot command executor with args validation rules

--- baby_yoda_bot/bot/bot.py
+++ baby_yoda_bot/bot/bot.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from baby_yoda_bot.models.AddressBook import AddressBook
 from baby_yoda_bot.utils import request_input, parse_input
+from baby_yoda_bot.exceptions.exceptions import ValidationValueException
 
 class Bot:
     __COMMANDS_METADATA__ = defaultdict(dict)
@@ -55,7 +56,73 @@ class Bot:
         # TODO: add notes class initialization
         self.address_book = AddressBook()
 
-   
+    def __exec(self, command, args):
+        if command not in Bot.__COMMANDS_METADATA__['map']:
+            return f"Unknown '{command}', use help to see commands list"
+
+        executor = Bot.__COMMANDS_METADATA__['map'][command]
+        metadata_key = executor.__bot_cmd__
+        metadata = Bot.__COMMANDS_METADATA__[metadata_key]
+
+        if metadata['args']:
+            if isinstance(metadata['args'], list):
+                validated_args = []
+
+                for rule in metadata['args']:
+                    is_optional = not rule['required'] if 'required' in rule else False
+
+                    while True:
+                        optional = '(optional)' if is_optional else ''
+                        value = input(f"Enter {rule['name']}{optional}: ")
+
+                        if not value and is_optional:
+                            value = None
+                            break
+
+                        if 'type' in rule:
+                            try:
+                                value = rule['type'](value)
+                                break
+                            except ValidationValueException as e:
+                                print(e)
+                                continue
+                
+                      
+                    validated_args.append(value)
+
+                args = validated_args
+
+            if isinstance(metadata['args'], str):
+                pass
+                # TODO: add args validation just by template like '<name> <phone> <birthday>
+
+        ctx = {
+            'address_book': self.address_book
+        }
+
+        return executor(ctx, args)
+
+    def listen(self):
+        # TODO: add hello message or animation
+
+        while True:
+            try:
+                command = request_input('Enter command: ')
+
+                if not command:
+                    continue
+
+                cmd, *args = parse_input(command)
+                output = self.__exec(cmd, args)
+
+                print(output)
+            except Exception as e:
+                print(e)
+                break
+            except KeyboardInterrupt:
+                print('\nBye!')
+                break
+
 
 __all__ = [
   'Bot'
