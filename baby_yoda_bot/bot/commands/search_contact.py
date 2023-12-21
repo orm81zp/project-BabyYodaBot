90f8fe3d6ed748b888ae31e02208607f431b74bc
antonina <atoninasych@gmail.com>
2023-12-21 11:37:01 +0000

    save/read contact

--- baby_yoda_bot/bot/commands/search_contact.py
+++ baby_yoda_bot/bot/commands/search_contact.py
@@ -8,13 +8,13 @@ from baby_yoda_bot.models import Name, Phone, Birthday, Email, Record, Context
 @Bot.description("used to search a contact")
 @Bot.questions(
     [
-        {"value": "value", "required": False, "type": str}
+        {"search": "search", "required": False, "type": str}
     ]
 )
 def search_contact(ctx: Context, args):
-    value = args
+    search = args
 
-    data = ctx.address_book.search(value)
+    data = ctx.address_book.search(search)
 
     return data
 
