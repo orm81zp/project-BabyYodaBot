b3c119ba401099b64daf84a1754e0f1556bc5d71
Roman Onishchenko <orm81zp@gmail.com>
2023-12-21 11:56:15 +0200

    fix note issues

--- baby_yoda_bot/models/Note.py
+++ baby_yoda_bot/models/Note.py
@@ -11,40 +11,37 @@ class Note:
         self.tags = []
         self.date_creation = time.strftime("%Y-%m-%d %H:%M:%S")
         self.date_modification = None
-    
-     # ----------- Tags------------------------------------------------
+
+    # ----------- Tags------------------------------------------------
     def add_tags(self, tags):
         self.tags.append(Tag(tags))
-    
-    def show_tags(self)
-        return self.tags
-    
-    def remove_tags(self):
-        self.tags = None
 
-    
-     # ----------- Content------------------------------------------------
+    def show_tags(self):
+        return self.tags
+
+    def remove_tags(self):
+        self.tags = []
+
+    # ----------- Content------------------------------------------------
     def add_content(self, content):
         self.content = Content(content)
 
     def show_content(self):
         return self.content
-    
+
     def remove_content(self):
         self.content = None
 
     # ----------- Content------------------------------------------------
-        
-    def add_title(self,title):
+
+    def add_title(self, title):
         self.title = Title(title)
 
     def show_title(self):
         return self.title
-    
+
     def remove_title(self):
         self.title = None
-   
-    def __str__(self):
-        return f"Title: {self.title.value}, Content: {content}, Tags: {self.tags.value}"
-        
 
+    def __str__(self):
+        return f'Title: {str(self.title)}, Content: {str(self.content)}, Tags: {", ".join([str(tag) for tag in self.tags])}'
